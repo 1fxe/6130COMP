2d484cb566485d81ad7c7295b2d02c8e51758576
Filip <filipwedzicha@gmail.com>
2023-02-11 18:24:43 +0000

    feat: Business tier add backend with basic POST validation
    Validates code and players name

---
+++ business/backend/src/runners_crisps.php
@@ -0,0 +1,36 @@
+<?php
+
+// Matches 10 digit hex code
+$CODE_REGEX = "/^[a-f0-9]{10}$/";
+
+
+// Sanatize the data remove spaces, slashes and special characters
+function sanatize($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// Check if the form has been submitted and if so, get the code and playername
+if (isset($_POST['submit'])) {
+    $code = strtolower(sanatize($_POST['code']));
+    $playerName = sanatize($_POST['playerName']);
+
+    // Check if the code is valid
+    if (!preg_match($CODE_REGEX, $code, )) {
+        echo "Invalid code";
+        exit();
+    }
+
+    // Check if the player name is valid
+    $nameLength = strlen($playerName);
+    if ($nameLength < 3 || $nameLength > 35) {
+        echo "Invalid player name";
+        exit();
+    }
+
+}
+
+?>
