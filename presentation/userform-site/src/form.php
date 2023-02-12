c3da904640538164b6a5f1372772430a76115ccb
Filip <filipwedzicha@gmail.com>
2023-02-12 16:52:17 +0000

    feat: Presentation tier improve form design and add missing name, email, adress
    Also split the html into template so the style can be reused

---
+++ presentation/userform-site/src/form.php
@@ -0,0 +1,24 @@
+<?php
+
+// Send the data from the HTML form to our backend, the load balancer is running on port 3000
+$response = file_get_contents("http://172.17.0.1:3000/backend.php?data=" . $_POST);
+
+$body = "";
+
+// The response returns "VOUCHER" is the user has won a voucher
+if ($response == "VOUCHER") {
+    $body = <<<HTML
+<h1>Congratulations you won a free football voucher! It will be emailed to you.</h1>
+HTML;
+} else if ($response === "DISCOUNT") {
+    $body = <<<HTML
+<h1>Congratulations you won a 10% off discount code! It will be emailed to you.</h1>
+HTML;
+}
+
+// Load our template
+$head = file_get_contents("head.html");
+$foot = file_get_contents("foot.html");
+
+echo $head . $body . $foot;
+?>
